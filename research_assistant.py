12ee3cd37f655667effebb84e04ab286b75ec7e4
Mukund Rakholiya <mrrakholiya1110@gmail.com>
2025-04-16 13:52:56 +0530

    Imported some dependancies

---
+++ research_assistant.py
@@ -0,0 +1,15 @@
+# Importing all the dependancies
+import os
+from dotenv import load_dotenv
+import hashlib
+from pypdf import PdfReader
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_groq import ChatGroq
+from langchain_chroma import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import pytesseract
+from pdf2image import convert_from_path
+import requests
+from bs4 import BeautifulSoup
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
