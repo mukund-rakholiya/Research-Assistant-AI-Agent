2ca370ffd710e36347a991d3fdfb2b7d9417357a
Mukund Rakholiya <mrrakholiya1110@gmail.com>
2025-04-17 10:47:06 +0530

    Configured streamlit page

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 from research_assistant import research_assistant, ask_question
 import os
+
+# configure page
+st.set_page_config(
+    page_title = "Research Assistant",
+    layout = "wide"
+)
+st.title("📚 Smart Research Assistant")
