4138c632f17e1a48a436cff338c1cfe90ea416f6
Mukund Rakholiya <mrrakholiya1110@gmail.com>
2025-04-17 10:49:34 +0530

    saving uploaded file

--- app.py
+++ app.py
@@ -20,7 +20,14 @@ with st.sidebar:
     citation_style = st.radio("Format:", ["APA", "MAL"], horizontal = True)
 
 if uploaded_file or url:
-    pass
+     # saving uploaded file
+    if (uploaded_file):
+        file_path = os.path.join("uploads", uploaded_file.name)
+        with open(file_path, "wb") as f:
+            f.write(uploaded_file.getbuffer())
+        input_source = file_path
+    else :
+        input_source = url
 
 else:
     st.info("Upload a PDF or enter URL to get started")
